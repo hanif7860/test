212615ed0f4e1f352d80eeca0140c2f860b9b3a0
hanif7860 <61457951+hanif7860@users.noreply.github.com>
2021-02-28 00:08:05 +0500

    Add files via upload

---
+++ DummyDotoList.js
@@ -0,0 +1,7 @@
+export const todoItems = [
+    { key: Math.random().toString(), data:"Shopping"},
+    { key: Math.random().toString(), data:"Excerise"},
+    { key: Math.random().toString(), data:"Prepare lunch"},
+    { key: Math.random().toString(), data:"Do the assignment"},
+  ]
+  
